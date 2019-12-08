a384958f32eeca11bb3a01beef39061d7de2dfc8
Lyla <LylaZhao@users.noreply.github.com>
2019-12-08 14:28:23 -0800

    Update python_code.py
    update the file as lab requirements - import pandas and numpy

--- python_code.py
+++ python_code.py
@@ -1,1 +1,4 @@
-print("Hello Github!")
+#This notebook is for the capstone project for Coursera Data Science program
+import pandas as pd
+import Numpy as np
+print("Hello Capstone Project Course!")
